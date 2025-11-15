c51f25fcc47dd0162b7e967d79ac1c154cafac36
David Faden (revfad.com) <dfaden@gmail.com>
2025-11-14 23:31:13 -0800

    Convert posts.json to CSV
    Also flatten the images into a single directory.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Convert posts.json + posts to CSV + flat images dir
+
+This utility takes the output from [gg_mhtml_to_site](https://github.com/fadend/gg_mhtml_to_site)
+and converts it into a CSV file containing both post metadata and content
+plus a flat directory of images.
+
+Renames images (changing references in the HTML) to avoid conflicts. The aim
+is to create something more easy to work with in importing the site into
+WordPress or similar.
+
+## Acknowledgments
+
+Used [poetry](https://python-poetry.org/) to generate package skeleton.
---
+++ src/gg_posts_csv_flat_images/make_gg_post_csv.py
@@ -0,0 +1,59 @@
+import argparse
+import csv
+import json
+import os
+import re
+import shutil
+
+OUTPUT_COLS = ("Title", "Content", "Date", "Image Featured", "Tags")
+
+def read_file(path) -> str:
+    with open(path, "rt", encoding="utf8") as f:
+        return f.read()
+
+def convert_to_csv(input_dir: str, output_dir: str, max_posts: int=-1):
+    with open(os.path.join(input_dir, "posts.json"), "rt", encoding="utf8") as f:
+        posts = json.load(f)
+    assert isinstance(posts, list)
+    if max_posts > 0:
+        posts = posts[:max_posts]
+    images_dir = os.path.join(output_dir, "images")
+    os.makedirs(images_dir, exist_ok=True)
+    with open(os.path.join(output_dir, "posts.csv"), "wt", encoding="utf8") as f:
+        w = csv.DictWriter(f, fieldnames=OUTPUT_COLS)
+        w.writeheader()
+        for post in posts:
+            sanitized_tags = [tag.replace("|", "") for tag in post["i_text"]]
+            row = {"Title": post["title"], "Date": post["post_date"], "Tags": "|".join(sanitized_tags)}
+            html_file = post["output_file"]
+            post_images_dir = post["images_dir"]
+            html = read_file(os.path.join(input_dir, html_file))
+            html = re.sub(r".*<body>\s*", "", html)
+            html = re.sub(r"\s*</body>.*", "", html)
+            first_image = ""
+            def move_image_match(m):
+                nonlocal first_image
+                src = m[1]
+                if not src.endswith((".jpeg", ".jpg")):
+                    return m[0]
+                if not src.startswith(post_images_dir):
+                    raise ValueError(f"{html_file} has src outside {post_images_dir}")
+                new_filename = src.replace("/", "_")
+                if not first_image:
+                    first_image = new_filename
+                shutil.copyfile(os.path.join(input_dir, src), os.path.join(images_dir, new_filename))
+                return new_filename
+            row["Content"] = re.sub(r'src="([^"]+)', move_image_match, html)
+            row["Image Featured"] = first_image
+            w.writerow(row)
+
+def main():
+    parser = argparse.ArgumentParser(prog="make_gg_post_csv")
+    parser.add_argument("--input_dir", required=True)
+    parser.add_argument("--output_dir", required=True)
+    parser.add_argument("--max_posts", type=int, default=-1)
+    args = parser.parse_args()
+    convert_to_csv(args.input_dir, args.output_dir, max_posts=args.max_posts)
+
+if __name__ == '__main__':
+    main()
